6d0fd7edaee01e5b50a4ccfee079ee62d4ad632a
geanemr <geaneramos1995@gmail.com>
2023-12-03 14:24:43 -0300

    chore: home

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+
+export const Btn = () => {
+  return (
+    <>
+     <button type="button" class="btn btn-outline-dark">Play</button>
+    </>
+  )
+}
---
+++ src/Components/Start/Start.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Start.css'
+import { Btn } from '../Button/Button'
+
+
+export const Start = () => {
+  return (
+    <div>
+    <h1>Get ready for this adventure in the Upside Down</h1>
+    <h2>How much do you know about the Stranger Things World? Let's find out!</h2>
+    <Btn />
+    </div>
+  )
+}
